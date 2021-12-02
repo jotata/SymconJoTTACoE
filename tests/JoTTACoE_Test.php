5653706869109c5a1b88be37767e23f4d580d6f0
jotata <admin@tanner-info.ch>
2021-12-03 00:24:31 +0100

    added testReceiveData

--- tests/JoTTACoE_Test.php
+++ tests/JoTTACoE_Test.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * @File:            JoTTACoE_Test.php
  * @Create Date:     13.11.2021 15:45:00
  * @Author:          Jonathan Tanner - admin@tanner-info.ch
- * @Last Modified:   13.11.2021 18:06:18
+ * @Last Modified:   03.12.2021 00:18:44
  * @Modified By:     Jonathan Tanner
  * @Copyright:       Copyright(c) 2020 by JoT Tanner
  * @License:         Creative Commons Attribution Non Commercial Share Alike 4.0
@@ -85,7 +85,7 @@ class JoTTACoE_Test extends TestCase {
 
     //Testet ob die Instanz erstellt werden kann
     public function testCreateInstance() {
-        IPS_CreateVariableProfile('~Switch', VARIABLETYPE_BOOLEAN); //Wird für Digitale VAriablen benötigt
+        IPS_CreateVariableProfile('~Switch', VARIABLETYPE_BOOLEAN); //Wird für Digitale Variablen benötigt
         $soID = IPS_CreateInstance($this->socketID);
         IPS_SetConfiguration($soID, json_encode(['Host' => '127.0.0.1', 'Open' => true, 'Port' => 5441]));
         IPS_ApplyChanges($soID);
@@ -97,12 +97,24 @@ class JoTTACoE_Test extends TestCase {
     //Testet Aufbereitung der Daten beim Empfang
     public function testReceiveData() {
         $iID = $this->testCreateInstance();
-        $conf = '{"RemoteIP":"127.0.0.1","RemoteNodeNr":10,"NodeNr":10,"Analog":"[{\"ID\":1,\"Ident\":\"A1\",\"Config\":3}]","Digital":"[{\"ID\":1,\"Ident\":\"D1\",\"Config\":3}]"}';
+        $analog = '[{\"ID\":1,\"Ident\":\"A1\",\"Config\":0},{\"ID\":2,\"Ident\":\"A2\",\"Config\":1},{\"ID\":3,\"Ident\":\"A3\",\"Config\":2},{\"ID\":4,\"Ident\":\"A4\",\"Config\":3}]';
+        $conf = '{"RemoteIP":"127.0.0.1","RemoteNodeNr":10,"NodeNr":10,"DisableReceiveDataFilter":1,"Analog":"' . $analog . '","Digital":"[{\"ID\":1,\"Ident\":\"D1\",\"Config\":3}]"}';
         IPS_SetConfiguration($iID, $conf);
         IPS_ApplyChanges($iID);
-        $data = '{"DataID":"{7A1272A4-CBDB-46EF-BFC6-DCF4A53D2FC7}","Type":0,"Buffer":"\n\u0001O\u0000U\u0001E\u0001\\\u0001\u0001\u0001\u0001\u0001","ClientIP":"127.0.0.1","ClientPort":5441}';
-        $res = true; //IPS_RequestAction($iID, 'ReceiveData', $data); //in Test-Stubs nicht implementirert
-        $this->assertTrue($res);
+
+        $this->assertFalse(@IPS_GetObjectIDByIdent("A1", $iID)); //Variable soll nicht erstellt sein
+        $this->assertGreaterThan(0, IPS_GetObjectIDByIdent("A2", $iID)); //Variable soll vorhanden sein (Aktiviert)
+        $this->assertGreaterThan(0, IPS_GetObjectIDByIdent("A3", $iID)); //Variable soll vorhanden sein (Eingang)
+        $this->assertGreaterThan(0, IPS_GetObjectIDByIdent("A4", $iID)); //Variable soll vorhanden sein (Ausgang)
+        
+        //Knoten 10 | Block 1 | A1=10.0°C | A2=10.0°C | A3=10.0°C | A4=10.0°C
+        $data = '{"DataID":"{7A1272A4-CBDB-46EF-BFC6-DCF4A53D2FC7}","Type":0,"Buffer":"\n\u0001d\u0000d\u0000d\u0000d\u0000\u0001\u0001\u0001\u0001","ClientIP":"127.0.0.1","ClientPort":5441}';
+        JoTTACoE_TestFunction($iID, 'ReceiveData', [$data]);
+        
+        $this->assertEquals(0, GetValue(IPS_GetObjectIDByIdent("A2", $iID))); //Variable darf nicht gefüllt werden (kein Eingang)
+        $this->assertEquals(10.0,  GetValue(IPS_GetObjectIDByIdent("A3", $iID))); //Variable = 10.0 (Eingang)
+        $this->assertEquals('JoTTACoE.Temperatur.1',  IPS_GetVariable(IPS_GetObjectIDByIdent("A3", $iID))['VariableProfile']); //Profil entspricht UnitID 1 (Temperatur in °C)
+        $this->assertEquals(0,  GetValue(IPS_GetObjectIDByIdent("A4", $iID))); //Variable darf nicht gefüllt werden (kein Eingang)
     }
 
     //Testet Aufbereitung der Daten beim Versand
